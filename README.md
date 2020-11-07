66ae88f3afead47e11bf1f7346f3ec93e8b1ad34
Suraj Mani <tech.smani@gmail.com>
2020-11-07 13:27:04 +0530

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# python-indian-states-quiz-game
+Build An Alexa Quiz Skill in Python using ASK Python SDK
+=============
+
+## What You Will Learn
+*  [AWS Lambda](http://aws.amazon.com/lambda)
+*  [Alexa Skills Kit (ASK)](https://developer.amazon.com/alexa-skills-kit)
+*  Using the [Skill builder (beta)](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/ask-define-the-vui-with-gui)
+*  Voice User Interface (VUI) Design
+
+## What You Will Need
+*  [Amazon Developer Portal Account](http://developer.amazon.com)
+*  [Amazon Web Services Account](http://aws.amazon.com/)
+*  [ASK Python SDK](https://github.com/alexa/alexa-skills-kit-sdk-for-python)
+*  A basic understanding of Python.
+
+## What Our Skill Will Do
+The Skill allows users to request a quiz about the 28 States of the INDIA. They will receive 10 random questions or they can ask for specific information, such as, "tell me about Gujrat".
+This simple "quiz" skill will teach you how the different pieces of the Alexa Skill development process fit together.
+
+We use will use the Skill builder UI to build the Skill although the quiz itself does not employ the new "Dialog-Delegate interface" model.
+
+### Concepts
+
+This skill shows how to use render template directives in the alexa 
+skill built using ASK Python SDK.
---
+++ lambda/alexa/data.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+
+STATES_LIST = [
+    {
+        'abbreviation': 'AP', 'capital': 'Amaravati',
+        'state': 'Andhra Pradesh', 'statehood_order': '1',
+        'statehood_year': '1956'
+    },
+    {
+        'abbreviation': 'AR', 'capital': 'Itanagar',
+        'state': 'Arunachal Pradesh', 'statehood_order': '2',
+        'statehood_year': '1987'
+    },
+    {
+        'abbreviation': 'AS', 'capital': 'Dispur',
+        'state': 'Assam', 'statehood_order': '3',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'BR', 'capital': 'Patna',
+        'state': 'Bihar', 'statehood_order': '4',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'CG', 'capital': '	Naya Raipur',
+        'state': 'Chhattisgarh', 'statehood_order': '5',
+        'statehood_year': '2000'
+    },
+    {
+        'abbreviation': 'GA', 'capital': '	Panaji',
+        'state': 'Goa', 'statehood_order': '6',
+        'statehood_year': '1987'
+    },
+    {
+        'abbreviation': 'GJ', 'capital': 'Gandhinagar',
+        'state': 'Gujarat', 'statehood_order': '7',
+        'statehood_year': '1960'
+    },
+    {
+        'abbreviation': 'HR', 'capital': 'Chandigarh',
+        'state': 'Haryana', 'statehood_order': '8',
+        'statehood_year': '1966'
+    },
+    {
+        'abbreviation': 'HP', 'capital': 'Shimla',
+        'state': 'Himachal Pradesh', 'statehood_order': '9',
+        'statehood_year': '1971'
+    },
+    {
+        'abbreviation': 'JH', 'capital': 'Ranchi',
+        'state': 'Jharkhand', 'statehood_order': '10',
+        'statehood_year': '2000'
+    },
+    {
+        'abbreviation': 'KA', 'capital': 'Bangalore',
+        'state': 'Karnataka', 'statehood_order': '11',
+        'statehood_year': '1956'
+    },
+    {
+        'abbreviation': 'KL', 'capital': 'Thiruvananthapuram',
+        'state': 'Kerala', 'statehood_order': '12',
+        'statehood_year': '1956'
+    },
+    {
+        'abbreviation': 'MP', 'capital': 'Bhopal',
+        'state': 'Madhya Pradesh', 'statehood_order': '13',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'MH', 'capital': 'Mumbai',
+        'state': 'Maharashtra', 'statehood_order': '14',
+        'statehood_year': '1960'
+    },
+    {
+        'abbreviation': 'MN', 'capital': 'Imphal',
+        'state': 'Manipur', 'statehood_order': '15',
+        'statehood_year': '1972'
+    },
+    {
+        'abbreviation': 'ML', 'capital': 'Shillong',
+        'state': 'Meghalaya', 'statehood_order': '26',
+        'statehood_year': '1972'
+    },
+    {
+        'abbreviation': 'MZ', 'capital': 'Aizawl',
+        'state': 'Mizoram', 'statehood_order': '27',
+        'statehood_year': '1963'
+    },
+    {
+        'abbreviation': 'NL', 'capital': 'Kohima',
+        'state': 'Nagaland', 'statehood_order': '28',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'OR', 'capital': 'Bhubaneswar',
+        'state': 'Odisha', 'statehood_order': '29',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'PB', 'capital': 'Chandigarh',
+        'state': 'Punjab', 'statehood_order': '30',
+        'statehood_year': '1966'
+    },
+    {
+        'abbreviation': 'RJ', 'capital': 'Jaipur',
+        'state': 'Rajasthan', 'statehood_order': '31',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'SK', 'capital': 'Gangtok',
+        'state': 'Sikkim', 'statehood_order': '32',
+        'statehood_year': '1975'
+    },
+    {
+        'abbreviation': 'TN', 'capital': 'Chennai',
+        'state': 'Tamil Nadu', 'statehood_order': '33',
+        'statehood_year': '1956'
+    },
+    {
+        'abbreviation': 'TG', 'capital': 'Hyderabad',
+        'state': 'Telangana', 'statehood_order': '34',
+        'statehood_year': '2014'
+    },
+    {
+        'abbreviation': 'TR', 'capital': 'Agartala',
+        'state': 'Tripura', 'statehood_order': '35',
+        'statehood_year': '1972'
+    },
+    {
+        'abbreviation': 'UP', 'capital': 'Lucknow',
+        'state': 'Uttar Pradesh', 'statehood_order': '36',
+        'statehood_year': '1950'
+    },
+    {
+        'abbreviation': 'UT', 'capital': 'Dehradun',
+        'state': 'Uttarakhand', 'statehood_order': '37',
+        'statehood_year': '2000'
+    },
+    {
+        'abbreviation': 'WB', 'capital': '	Kolkata',
+        'state': 'West Bengal', 'statehood_order': '38',
+        'statehood_year': '1950'
+    }]
+
+SKILL_TITLE = "Indian States Quiz Game"
+
+WELCOME_MESSAGE = ("Welcome to the Indian States Quiz Game!  "
+                   "You can ask me about any of the "
+                   "twenty eight states and their capitals, or you can ask me to "
+                   "start a quiz.  What would you like to do? ")
+
+START_QUIZ_MESSAGE = ("OK.  I will ask you 10 questions about the "
+                      "Indian States. ")
+
+EXIT_SKILL_MESSAGE = ("Thank you for playing the Indian States Quiz Game!  "
+                      "Let's play again soon!")
+
+REPROMPT_SPEECH = "Which other state or capital would you like to know about?"
+
+HELP_MESSAGE = ("I know lots of things about the Indian States.  "
+                "You can ask me about a state "
+                "or a capital, and I'll tell you what I know.  "
+                "You can also test your knowledge by asking me to start "
+                "a quiz.  What would you like to do? ")
+
+CORRECT_SPEECHCONS = ['Booya', 'All righty', 'Bam', 'Bazinga', 'Bingo',
+                      'Boom', 'Bravo', 'Cha Ching', 'Cheers', 'Dynomite',
+                      'Hip hip hooray', 'Hurrah', 'Hurray', 'Huzzah',
+                      'Oh dear.  Just kidding.  Hurray', 'Kaboom', 'Kaching',
+                      'Oh snap', 'Phew', 'Righto', 'Way to go', 'Well done',
+                      'Whee', 'Woo hoo', 'Yay', 'Wowza', 'Yowsa']
+
+WRONG_SPEECHCONS = ['Argh', 'Aw man', 'Blarg', 'Blast', 'Boo', 'Bummer',
+                    'Darn', "D'oh", 'Dun dun dun', 'Eek', 'Honk', 'Le sigh',
+                    'Mamma mia', 'Oh boy', 'Oh dear', 'Oof', 'Ouch', 'Ruh roh',
+                    'Shucks', 'Uh oh', 'Wah wah', 'Whoops a daisy', 'Yikes']
+
+IMG_PATH = (
+    "https://m.media-amazon.com/images/G/01/mobile-apps/dex/alexa/"
+    "alexa-skills-kit/tutorials/quiz-game/state_flag/{}x{}/{}._TTH_.png")
+
+USE_CARDS_FLAG = True
+
+MAX_QUESTIONS = 10
+
+BAD_ANSWER = (
+    "I'm sorry. {} is not something I know very much about in this skill.")
+
+FALLBACK_ANSWER = (
+    "Sorry. I can't help you with that. {}".format(HELP_MESSAGE))
+
+SCORE = "Your {} score is {} out of {}. "
+
+SPEECH_DESC = (
+    "{} is the {}th state, "
+    "admitted to the Union in {}.  "
+    "The capital of {} is {}, and the "
+    "abbreviation for {} is "
+    "<break strength='strong'/><say-as interpret-as='spell-out'>"
+    "{}</say-as>.  I've added {} to "
+    "your Alexa app.  Which other state or capital would you like to "
+    "know about? ")
